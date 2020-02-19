1bdd747ee61d16d400741c5b2be6bcb9f4d7406a
ThanhNgo <thanhngo26@hotmail.com>
2020-02-18 16:42:49 -0800

    router created

--- server/index.js
+++ server/index.js
@@ -6,11 +6,17 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
+//require router
+const router = require('./router');
+
 //Setting up socket.io
 //https://socket.io/docs/#Using-with-Node-http-server
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+//Call router as a middleware
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
