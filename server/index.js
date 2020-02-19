ffb43a4c4a7f6777688d3882b1fb625d9d6a37d3
ThanhNgo <thanhngo26@hotmail.com>
2020-02-18 17:44:50 -0800

    socket.io instance created for connection and disconnetion

--- server/index.js
+++ server/index.js
@@ -13,10 +13,31 @@ const router = require('./router');
 //https://socket.io/docs/#Using-with-Node-http-server
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server);//socket.io instance created => this is the backend
+
+// https://socket.io/get-started/chat/
+//This will run when we have a client connection on our io instance when client joining and leaving our chat room
+io.on('connection', (socket) => {//(socket) will connected as a client side socket
+    console.log('Connection is on....');
+
+    //disconnect, no socket parameter needed because client left
+    socket.on('disconnect', () => {
+        console.log('User had left!');
+    })
+});
+
 
 //Call router as a middleware
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
+
+// https://socket.io/get-started/chat/
+//This will run when we have a client connection on our io instance when client joining and leaving our chat room
+// io.on('connection', function(socket){
+//     console.log('a user connected');
+//     socket.on('disconnect', function(){
+//       console.log('user disconnected');
+//     });
+//   });
