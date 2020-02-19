e607579dd31300884be999617b1e80ab3daeb09a
ThanhNgo <thanhngo26@hotmail.com>
2020-02-18 19:23:56 -0800

    Join Application created

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,7 +1,27 @@
-import React from 'react';
+//{useState} => This is a hooks for using state inside of the function for based components
+import React, { useState } from 'react';
+
+import { Link } from 'react-router-dom';//this link is used to link to our /chat path
+
+import './Join.css';
+
+//declare hooks inside of the function based components
+//name, setter function to pass name state
+//room, setter function to pass room state
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
     return (
-        <h1>Join</h1>
+       <div className="joinOuterContainer">
+           <div className="joinInnerContainer">
+               <h1 className="heading">Join</h1>
+               <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} /></div>
+               <div><input placeholder="Name" className="joinInput nt-20" type="text" onChange={(event) => setRoom(event.target.value)} /></div>
+               <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to ={`/chat?name=${name} & room=${room}`}>
+                    <button className="button mt-20" type="submit">Sign In</button>
+               </Link>
+           </div>
+       </div>
     )
 }
 export default Join;
