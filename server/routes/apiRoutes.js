babf2bd2dc105ad8f78c582cf032576a3083fa35
Pedja <vpedja12@gmail.com>
2020-02-29 13:37:37 -0800

    route is working

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,8 +1,15 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
 const Messages = require("../models/Messages");
 
+//get request from the root route
+router.get('/', (req, res) => {
+    res.send('Server is running...');
+});
+
 router.get("/api/chat", (req, res) => {
-  Messages.find({})
+    console.log("api/chat");
+  Messages.findall({})
     .then(msg => {
       res.json(msg);
       console.log(msg);
