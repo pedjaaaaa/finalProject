9070b8d27ff8cc1e85291ff935463bdf03f72334
Pedja <vpedja12@gmail.com>
2020-03-03 21:33:46 -0800

    working version

--- client/src/utils/GiphyAPI.js
+++ client/src/utils/GiphyAPI.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-const BASEURL = "api.giphy.com/v1/gifs/search/";
-const APIKey = "J5dNYggytT7tnI3dF9pmQCVZpg8Btui3";
+const BASEURL = "https://api.giphy.com/v1/gifs/random?api_key=J5dNYggytT7tnI3dF9pmQCVZpg8Btui3&tag=";
+// const APIKey = "J5dNYggytT7tnI3dF9pmQCVZpg8Btui3";
 
 
 export default {
   search: function(content) {
-    return axios.get(BASEURL + content + APIKey);
+    return axios.get(BASEURL + content);
   }
 };
